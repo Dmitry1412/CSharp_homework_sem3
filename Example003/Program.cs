ac133f130179fec54848c95e43c908fdfc1fff8e
Dmitry <sicily007@rambler.ru>
2023-08-26 23:25:19 +0300

    third_example

---
+++ Example003/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Внутри класса Answer напишите метод ShowCube, 
+который принимает на вход число (N)
+и выводит таблицу кубов чисел от 1 до N (включительно) каждое на новой строке.*/
+
+
+void ShowCube(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        System.Console.WriteLine(Math.Pow(i, 3));
+    }
+    
+}
+//ShowCube(5);
